c0fc988f166b2871e75b46c36107062980e46aa1
Blair Rush <airblair94@yahoo.com>
2014-06-21 15:59:07 -0400

    setup for similarity network

---
+++ TempScripts_BG/similarity.py
@@ -0,0 +1,10 @@
+import get_data as gd
+
+headers = gd.get_headers()
+dicts = gd.get_data_list_of_dicts()
+
+rows_lol = []
+for i in range(len(gd.get_data_slice(headers[0], dicts):
+	rows_lol.append([])
+
+print len rows_lol
